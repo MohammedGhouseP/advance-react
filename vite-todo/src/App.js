e7ee0a77fd4edc7873e391fc1c8e5eb8cdf9fb04
MohammedGhouseP <ghousebashamail@gmail.com>
2024-06-02 00:26:54 +0530

    todo

---
+++ vite-todo/src/App.js
@@ -0,0 +1,12 @@
+import Todo from './Todo.jsx'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Todo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite-todo/src/Todo.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (inputValue.trim() !== '') {
+      setTodos([...todos, inputValue]);
+      setInputValue('');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
