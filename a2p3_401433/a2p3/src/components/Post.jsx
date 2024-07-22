a11f65efac50875a0fc189d5fed976d88465802c
MohammedGhouseP <ghousebashamail@gmail.com>
2024-07-23 00:59:50 +0530

    Fetching and display data with axios

---
+++ a2p3_401433/a2p3/src/components/Post.jsx
@@ -0,0 +1,12 @@
+function Post({ title, body }) {
+  return (
+    <div>
+      <p><b>Title : </b> {title}</p>
+      <p><b>Body : </b> {body}</p>
+      <br/>
+      <hr></hr>
+    </div>
+  );
+}
+
+export default Post;
