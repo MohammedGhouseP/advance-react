8aa698dc5ed30795494e98bf78eeeb5a56f0eb7e
MohammedGhouseP <ghousebashamail@gmail.com>
2024-07-23 23:46:30 +0530

    useContext

---
+++ a3p3_354722/a3p3/src/context/AuthContextProvider.jsx
@@ -0,0 +1,36 @@
+import { createContext, useState } from "react";
+
+// Step 1: Create Context
+export const AuthContext = createContext();
+
+// Step 2.A : Provide Context
+export function AuthContextProvider(props) {
+  // State to manage loggedin status
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // Function to log in
+
+  const login = () => {
+    setIsLoggedIn(true);
+    console.log("logIn")
+  }
+
+  // Function to log out
+
+  const logout = () => {setIsLoggedIn(false)
+    console.log("logOut")
+  }
+
+  return (
+    <AuthContext.Provider
+      // Providing authentication state and functions to children components
+      value={{
+        isLoggedIn,
+        login,
+        logout,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+}
